0009fd1892a8c76a878c0f3a0160df9344800977
wuys <ngo@wuys.me>
2023-02-08 10:16:58 +0700

    Add yarn

--- wsl/setup.sh
+++ wsl/setup.sh
@@ -5,12 +5,13 @@ dnf update -y
 
 # Set up repo
 curl -fsSL https://rpm.nodesource.com/setup_18.x | bash -
+curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
 
 # Require packages
 dnf install -y epel-release
 dnf install -y ncurses wget unzip zip gzip htop neovim procps git util-linux-user fish
-dnf groupinstall -y "Development Tool"
-dnf install -y nodejs 
+dnf groupinstall -y "Development Tools"
+dnf install -y nodejs yarn
 
 # Enable systemd
 cat > "/etc/wsl.conf" << END
