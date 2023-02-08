0940ddb646d1e4b718ced50e04353c2512eb2a7e
wuys <ngo@wuys.me>
2023-02-08 10:25:05 +0700

    Fix add yarn repo

--- wsl/setup.sh
+++ wsl/setup.sh
@@ -5,11 +5,11 @@ dnf update -y
 
 # Set up repo
 curl -fsSL https://rpm.nodesource.com/setup_18.x | bash -
-curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | sudo tee /etc/yum.repos.d/yarn.repo
+curl -sL https://dl.yarnpkg.com/rpm/yarn.repo | tee /etc/yum.repos.d/yarn.repo
 
 # Require packages
 dnf install -y epel-release
-dnf install -y ncurses wget unzip zip gzip htop neovim procps git util-linux-user fish
+dnf install -y sudo procps util-linux-user ncurses wget unzip zip gzip htop neovim git fish
 dnf groupinstall -y "Development Tools"
 dnf install -y nodejs yarn
 
